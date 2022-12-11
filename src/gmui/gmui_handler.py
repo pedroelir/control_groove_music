5cfe18db81c5fc92a5bc8db3cb82214a472eba5d
pedroelir <pedro_r_b777@hotmail.com>
2022-12-11 18:42:23 +0100

    uncomment code to add folder, move criteria to global

--- src/gmui/gmui_handler.py
+++ src/gmui/gmui_handler.py
@@ -6,6 +6,40 @@ from pywinauto.base_wrapper import BaseWrapper
 from pywinauto import Desktop, findwindows, WindowSpecification
 import subprocess
 
+choose_folder_criteria: Dict[str, str] = {
+    "title": "Not finding everything?, Show us where to look for music, ",
+    "auto_id": "NotificationBannerButton",
+    "control_type": "Button",
+}
+songs_submenu_criteria: Dict[str, str] = {"title_re": "Songs", "control_type": "ListItem"}
+artists_submenu_criteria: Dict[str, str] = {
+    "title_re": "Artists",
+    "control_type": "ListItem",
+}
+albums_submenu_criteria: Dict[str, str] = {
+    "title_re": "Albums",
+    "control_type": "ListItem",
+}
+
+addfolder_add_button_criteria: Dict[str, str] = {"title": "Add folder", "control_type": "ListItem"}
+addfolder_done_button_criteria: Dict[str, str] = {
+    "title": "Done",
+    "auto_id": "PrimaryButton",
+    "control_type": "Button",
+}
+
+addfolderw_folder_textbox_criteria: Dict[str, str] = {
+    "title": "Folder:",
+    "auto_id": "1152",
+    "control_type": "Edit",
+}
+addfolderw_addthisfolder_button_criteria: Dict[str, str] = {
+    "title": "Add this folder to Music",
+    "auto_id": "1",
+    "control_type": "Button",
+}
+
+songlist_crtieria: Dict[str, str] = {"title": "MY MUSIC", "auto_id": "SongsList", "control_type": "List"}
 
 mute_button_criteria: Dict[str, str] = {
     "title_re": "Mute",
@@ -63,58 +97,27 @@ class GM_UI:
         if type(media_path) == str:
             media_path: Path = Path(media_path)
 
-        # choose_folder_criteria: Dict[str:str] = {
-        #     "title": "Not finding everything?, Show us where to look for music, ",
-        #     "auto_id": "NotificationBannerButton",
-        #     "control_type": "Button",
-        # }
-        # self.window.child_window(**choose_folder_criteria).click_input()
+        self.window.child_window(**choose_folder_criteria).click_input()
 
-        # addfolder_add_button: Dict[str:str] = {"title": "Add folder", "control_type": "ListItem"}
-        # self.window.child_window(**addfolder_add_button).click_input()
+        self.window.child_window(**addfolder_add_button_criteria).click_input()
 
-        # addfolderw_folder_textbox_criteria: Dict[str, str] = {
-        #     "title": "Folder:",
-        #     "auto_id": "1152",
-        #     "control_type": "Edit",
-        # }
-        # self.window.child_window(**addfolderw_folder_textbox_criteria).type_keys(media_path.parent.absolute())
+        self.window.child_window(**addfolderw_folder_textbox_criteria).type_keys(media_path.parent.absolute())
 
-        # addfolderw_addthisfolder_button_criteria: Dict[str, str] = {
-        #     "title": "Add this folder to Music",
-        #     "auto_id": "1",
-        #     "control_type": "Button",
-        # }
-        # self.window.child_window(**addfolderw_addthisfolder_button_criteria).click_input()
+        self.window.child_window(**addfolderw_addthisfolder_button_criteria).click_input()
 
-        # addfolder_done_button_criteria: Dict[str:str] = {
-        #     "title": "Done",
-        #     "auto_id": "PrimaryButton",
-        #     "control_type": "Button",
-        # }
-        # time.sleep(1)
-        # self.window.child_window(**addfolder_done_button_criteria).click_input()
+        time.sleep(1)
+        self.window.child_window(**addfolder_done_button_criteria).click_input()
 
-        # # Rotate through Artis/AlmunSongs to ensure refresh
-        # songs_submenu_criteria: Dict[str, str] = {"title_re": "Songs", "control_type": "ListItem"}
-        # artists_submenu_criteria: Dict[str, str] = {
-        #     "title_re": "Artists",
-        #     "control_type": "ListItem",
-        # }
-        # albums_submenu_criteria: Dict[str, str] = {
-        #     "title_re": "Albums",
-        #     "control_type": "ListItem",
-        # }
-        # self.window.child_window(**artists_submenu_criteria).click_input()
-        # self.window.child_window(**albums_submenu_criteria).click_input()
-        # self.window.child_window(**songs_submenu_criteria).click_input()
+        # Rotate through Artis/Album/Songs to ensure refresh
+        self.window.child_window(**artists_submenu_criteria).click_input()
+        self.window.child_window(**albums_submenu_criteria).click_input()
+        self.window.child_window(**songs_submenu_criteria).click_input()
 
         # get song list
-        songlist_crtieria: Dict[str, str] = {"title": "MY MUSIC", "auto_id": "SongsList", "control_type": "List"}
         songlist_parent: BaseWrapper = self.window.child_window(**songlist_crtieria).wrapper_object()
 
         songlist_descendants: List[BaseWrapper] = songlist_parent.descendants()
-        songlist: List[str] = [
+        songsdescendants: List[str] = [
             element.element_info.name
             for element in songlist_descendants
             if element.element_info.automation_id == "Title"
@@ -126,19 +129,19 @@ class GM_UI:
                 "Albums",
             ]
         ]
-        print(f"{songlist=}")
+        print(f"{songsdescendants=}")
 
-        children: List[BaseWrapper] = songlist_parent.iter_children()
-        songchildren: List[str] = []
-        for child in children:
+        songs_list: List[BaseWrapper] = songlist_parent.iter_children()
+        songs: List[str] = []
+        for child in songs_list:
             if child.element_info.control_type == "ListItem":
-                songchildren.append(child.children()[0].element_info.name)
+                songs.append(child.children()[0].element_info.name)
                 # for grandchild in child.children():
                 #     if grandchild.element_info.automation_id == "Title":
                 #         songchildren.append(grandchild.element_info.name)
 
-        print(f"{children=}")
-        print(f"{songchildren=}")
+        print(f"{songs_list=}")
+        print(f"{songs=}")
 
         def get_best_match(candidates: List[str], reference: str, threshold: float = 0.5) -> str:
             from difflib import SequenceMatcher
@@ -160,7 +163,7 @@ class GM_UI:
             return best_match
 
         reference: str = media_name if media_name is not None else media_path.stem
-        song_title: str = get_best_match(songchildren, reference)
+        song_title: str = get_best_match(songs, reference)
 
         song_text_criteria: Dict[str, str] = {"title": song_title, "control_type": "Text"}
         playall_button_criterria: Dict[str, str] = {
